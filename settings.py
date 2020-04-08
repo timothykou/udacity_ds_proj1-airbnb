9f6ef9ee2bf6443d636c605c1205487cf1012063
timothy.kou <timothy.kou@iexcloud.io>
2020-04-07 23:53:33 -0400

    removed files

--- settings.py
+++
@@ -1,5 +0,0 @@
-import os
-
-AIRBNB_DATA_LINKS = 'http://insideairbnb.com/get-the-data.html'
-MEDIAN_PRICES_PATH = os.path.dirname(os.path.abspath(__file__)) + '/Data/median_prices.json'
-URLS_USED_PATH = os.path.dirname(os.path.abspath(__file__)) + '/Data/urls_used.json'
